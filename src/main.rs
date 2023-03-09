846602463883feb28dcb2598f3bd92bcdaf9654f
anish <anish@noreply.com>
2023-03-09 17:31:27 +0530

    Added ds for the User Input

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,56 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::thread;
-use std::time::Duration;
-use std::sync::{Arc, Mutex};
-use std::io::{self, BufRead};
+use std::time::{Duration, SystemTime};
 
 fn main() {
-    // Define a shared vector to hold programs and their intervals
-    let programs = Arc::new(Mutex::new(vec![
-        ("Program A".to_string(), Duration::from_secs(60)),
-        ("Program B".to_string(), Duration::from_secs(600 * 60 * 5)),
-    ]));
+    // Read program intervals from a file
+    let intervals = read_intervals_from_file("./program_intervals.txt").unwrap();
 
-    // Spawn a thread to listen for user input to add new programs
-    let programs_clone = programs.clone();
-    thread::spawn(move || {
-        loop {
-            // Read user input
-            let stdin = io::stdin();
-            let line = stdin.lock().lines().next().unwrap().unwrap();
-            let tokens: Vec<&str> = line.split(" ").collect();
+    // Start a thread for each program
+    let mut threads = vec![];
+    for (program, url, method, parameter, interval) in intervals {
+        let thread = thread::spawn(move || {
+            loop {
+                let start_time = SystemTime::now();
+                // Execute the program
+                execute_program(&program, &url, &method, &parameter);
 
-            // Parse user input to add new programs
-            if tokens[0] == "add" && tokens.len() == 3 {
-                let program_name = tokens[1].to_string();
-                let interval = match tokens[2].parse::<u64>() {
-                    Ok(n) => Duration::from_secs(n),
-                    Err(_) => {
-                        println!("Invalid interval specified.");
-                        continue;
-                    }
-                };
-
-                let mut programs = programs_clone.lock().unwrap();
-                programs.push((program_name, interval));
-                println!("Program added.");
-            } else {
-                println!("Invalid command.");
+                // Wait until the next interval
+                let elapsed_time = start_time.elapsed().unwrap().as_secs();
+                let remaining_time = interval - elapsed_time % interval;
+                thread::sleep(Duration::from_secs(remaining_time));
             }
-        }
-    });
+        });
+        threads.push(thread);
+    }
 
-    // Loop through each program and run it on its specified interval
-    loop {
-        let programs = programs.lock().unwrap();
-        for (program_name, interval) in programs.iter() {
-            let program_name_clone = program_name.clone();
-            let interval_clone = *interval;
-            thread::spawn(move || {
-                loop {
-                    // Run the program's function
-                    run_program(&program_name_clone);
-
-                    // Wait for the specified interval
-                    thread::sleep(interval_clone);
-                }
-            });
-        }
-
-        // Wait for all threads to finish
-        thread::park_timeout(Duration::from_secs(1));
+    // Wait for all threads to finish
+    for thread in threads {
+        thread.join().unwrap();
     }
 }
 
-// Function that runs the program
-fn run_program(program_name: &str) {
-    println!("Running {}...", program_name);
-    // Add code here to run the program's functionality
+fn read_intervals_from_file(filename: &str) -> Result<Vec<(String, String, String, String, u64)>, std::io::Error> {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+    let mut intervals = vec![];
+    for line in reader.lines() {
+        let line = line?;
+        let parts: Vec<&str> = line.split(',').collect();
+        if parts.len() == 5 {
+            let program = parts[0].trim().to_string();
+            let url = parts[1].trim().to_string();
+            let method = parts[2].trim().to_string();
+            let parameter = parts[3].trim().to_string();
+            let interval = parts[4].trim().parse().unwrap();
+            intervals.push((program, url, method, parameter, interval));
+        }
+    }
+    Ok(intervals)
+}
+
+fn execute_program(program: &String, x: &String, x0: &String, x1: &String) {
+    // Execute the program here
+    println!("Executing program: {}", program);
 }
