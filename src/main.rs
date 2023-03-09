07ad9a070540bb339c575c9137bd57b7aa57603a
anish <anish@noreply.com>
2023-03-09 20:22:32 +0530

    Added web request handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::thread;
 use std::time::{Duration, SystemTime};
-
+use reqwest::{Error, Client};
+use serde_json::{json, Value};
 
 fn main() {
     // Read program intervals from a file
@@ -52,18 +53,36 @@ fn read_intervals_from_file(filename: &str) -> Result<Vec<(String, String, Strin
     Ok(intervals)
 }
 
-fn execute_program(program: &String, url: &String, method: &String, parameter: &String , timeout: &u64) {
+fn execute_program(program: &String, url: &String, method: &String, parameter: &String, timeout: &u64) {
+
+    println!(
+        "Executing program: {} {} {} {} {}",
+        program, url, method, parameter, timeout
+    );
     // If method is GET, perform an HTTP GET request
     if method.to_uppercase() == "GET" {
-        perform_get_request(url, timeout);
-    }
-    // Otherwise, execute the program here
-    else {
-        println!("Executing program: {} {} {} {} {}", program , url , method , parameter , timeout );
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        match rt.block_on(perform_get_request(url, timeout)) {
+            Ok(response) => {
+                if response.status().is_success() {
+                    println!("Successfully performed GET request. Response body:");
+                } else {
+                    println!("Error performing GET request. Status code: {}", response.status());
+                    if let Err(e) = send_webhook(webhook_url, &response).await {
+                        println!("Error sending webhook: {}", e);
+                    }
+                }
+            },
+            Err(e) => println!("Error performing GET request: {}", e),
+        }
     }
 }
 
-async fn perform_get_request(url: &String, timeout: &u64) -> Result<(), reqwest::Error> {
+
+async fn perform_get_request(url: &String, timeout: &u64) -> Result<reqwest::Response, reqwest::Error> {
     // Create a new reqwest client
     let client = reqwest::Client::new();
 
@@ -74,10 +93,36 @@ async fn perform_get_request(url: &String, timeout: &u64) -> Result<(), reqwest:
         .send()
         .await?;
 
-    // Print out the response body
-    let body = response.text().await?;
-    println!("{}", body);
-
-    Ok(())
+    Ok(response)
 }
 
+// async fn send_discord_webhook(program: &str, url: &str, method: &str, timeout: &u64) -> Result<(), Error> {
+//     let webhook_url = "https://discord.com/api/webhooks/1234567890123456789/AbCdEfGhIjKlMnOpQrStUvWxYz";
+//     let message = "Hello, world!";
+//     let client = Client::new();
+//     let payload = json!({ "content": message });
+//     let res = client.post(webhook_url)
+//         .json(&payload)
+//         .send()
+//         .await?;
+//     let response_text = response.text().await?;
+//     let response_json: Value = serde_json::from_str(&response_text)
+//         .map_err(|err| reqwest::Error::from(err))?;
+//     if response_json["code"] == 204 {
+//         Ok(())
+//     } else {
+//         //println!("Sorry cant post ")
+//         //Err(Error::from(format!("Discord webhook returned code {}", response_json["code"])))
+//         Err(reqwest::Error::from(format!("Discord webhook returned code {}", response_json["code"])))
+//
+//     }
+// }
+
+fn send_webhook(webhook_url: &str, response: &reqwest::Response) -> Result<(), reqwest::Error> {
+    let status_code = response.status();
+    let response_body = response.text()?;
+    let message = format!("Error performing GET request. Status code: {}. Response body: {}", status_code, response_body);
+    let client = Client::new();
+    client.post(webhook_url).json(&message).send()?;
+    Ok(())
+}
